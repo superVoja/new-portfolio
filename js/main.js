35625401a2e74fe72cf7ad339694d592518d3d20
Vojislav <vojislav.sibinovic@gmail.com>
2019-09-30 13:20:24 +0200

    Updates

---
+++ js/main.js
@@ -0,0 +1,49 @@
+//Menu Navigation
+const menuBtn = document.querySelector(".menu-button");
+const menu = document.querySelector(".overlay-menu");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".menu-item");
+
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
+    showMenu = false;
+  }
+}
+
+//Sticky Header
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".section-one");
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
