076a131fefa030ac7b6de57e0136bfa44384b71c
Vojislav <vojislav.sibinovic@gmail.com>
2019-09-30 13:19:29 +0200

    Sticky header , client logos . resume and other small changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,4 @@
+//Menu Navigation
 const menuBtn = document.querySelector(".menu-button");
 const menu = document.querySelector(".overlay-menu");
 const menuNav = document.querySelector(".menu-nav");
@@ -22,3 +23,27 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+//Sticky Header
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".section-one");
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
